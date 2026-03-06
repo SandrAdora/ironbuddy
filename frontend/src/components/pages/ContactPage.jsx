d6a78e9f9e9ba58caf5ddf1958e5b8fc05150028
SandrAdora <sandraedigin@yahoo.com>
2026-03-06 17:38:37 +0100

    updated the navbar and the contactpage as well as the homepage

--- frontend/src/components/pages/ContactPage.jsx
+++ frontend/src/components/pages/ContactPage.jsx
@@ -1,9 +1,7 @@
 
 export default function Contact() {
   return (
-    <div className="bg-[--color-gym-dark] text-white mt-40 ">
-
-      
+    <div className="min-h-screen bg-[--color-gm-dark] text-white mt-40 ">
       <div className=" text-[--color-iron-gold] drop-shadow-[0_0_10px_rgba(250,204,21,0.8)] animate-coach-breathe">
         <div className="bg-gray-900 p-8 rounded-lg shadow-lg">
           <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
